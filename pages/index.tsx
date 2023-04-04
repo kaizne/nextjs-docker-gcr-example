5bf1c26b9b64dfcca9db70999a4816c2dfa2a99a
Kaizne <kaizne@kaizne.com>
2023-04-04 03:02:37 -0700

    Test cloudbuild

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Hello! I am being deployed again!!!&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
